fa3bd9a672f22475186fe7c3585d189765a73a83
MostakemHossain <mostakememon123@gmail.com>
2024-08-11 09:51:11 +0600

    auth,jwt helpers,appError added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
+import notFound from "./app/middleware/notFound";
 import router from "./app/routes";
 
 // express
@@ -19,5 +20,8 @@ app.get("/", (req: Request, res: Response) => {
 
 // global error handler
 app.use(globalErrorHandler);
+
+// not found route
+app.use(notFound);
 
 export default app;
---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import config from "../config";
+
+import AppError from "../errors/appError";
+import { jwtHelpers } from "../helpers/jwtHelper";
+
+const auth = (...roles: string[]) => {
+  return async (
+    req: Request & { user?: any },
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new AppError(httpStatus.BAD_REQUEST, "You are not authorized");
+      }
+      const verifiedUser = await jwtHelpers.verifyToken(
+        token,
+        config.jwt__access_secret as string
+      );
+      req.user = verifiedUser;
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new AppError(httpStatus.BAD_REQUEST, "You are not authorized");
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default auth;
