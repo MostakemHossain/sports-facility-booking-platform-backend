5cfcdb7fce0effdf2ba73ad2cdfd82bac3eee3ec
MostakemHossain <mostakememon123@gmail.com>
2024-08-11 18:00:35 +0600

    handleCast error

---
+++ src/app/errors/handleDuplicateError.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TErrorSource, TGenericErrorResponse } from "../../interface/error";
+
+const handleDuplicateError = (err: any): TGenericErrorResponse => {
+  const match = err.message.match(/"([^"]*)"/);
+  const extractedMessage = match && match[1];
+  const errorSources: TErrorSource = [
+    {
+      path: "",
+      message: `${extractedMessage} is Already exists`,
+    },
+  ];
+  const statusCode = 400;
+  return {
+    statusCode,
+    message: extractedMessage,
+    errorSources,
+  };
+};
+export default handleDuplicateError;
