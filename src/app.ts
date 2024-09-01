da9e106ac3ece0b0e98f3c016c868494551d5c87
MostakemHossain <mostakememon123@gmail.com>
2024-09-01 20:56:59 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,14 @@ const app = express();
 
 // parsers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://sports-edge.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  })
+);
 app.use(cookieParser());
 
 // application routes
